e05c57fe34dd0380a14757f612c5d3f64d92657f
Joel Arvidsson <joel.arvidsson@klarna.com>
2021-05-20 15:25:42 +0200

    Add support for resizeMode prop

--- src/index.d.ts
+++ src/index.d.ts
@@ -6,6 +6,7 @@ export interface VectorDrawableProps extends ViewProps {
    * The name of the vector drawable resource.
    */
   resourceName: string;
+  resizeMode?: 'contain' | 'cover' | 'stretch' | 'center';
 }
 
 declare const VectorDrawable: React.FunctionComponent<VectorDrawableProps>;
