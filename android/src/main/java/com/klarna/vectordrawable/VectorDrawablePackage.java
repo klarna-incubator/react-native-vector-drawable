2dbdab0cb19e16e6143533e89e57a8788cee0638
Joel Arvidsson <joel@oblador.se>
2023-10-25 15:27:20 +0200

    Support Fabric on React Native 0.72

--- android/src/main/java/com/klarna/vectordrawable/VectorDrawablePackage.java
+++ android/src/main/java/com/klarna/vectordrawable/VectorDrawablePackage.java
@@ -1,32 +1,26 @@
 package com.klarna.vectordrawable;
 
-import androidx.annotation.NonNull;
-
 import com.facebook.react.ReactPackage;
 import com.facebook.react.bridge.NativeModule;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.uimanager.ViewManager;
-import com.facebook.soloader.SoLoader;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-@SuppressWarnings("unused")
 public class VectorDrawablePackage implements ReactPackage {
+
     @Override
-    @NonNull
-    public List<NativeModule> createNativeModules(@NonNull final ReactApplicationContext reactContext) {
-        if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
-            // TODO: Remove when auto-linking will support Fabric modules
-            // See: https://github.com/software-mansion/react-native-screens/blob/main/android/src/main/java/com/swmansion/rnscreens/RNScreensPackage.kt#L16
-            SoLoader.loadLibrary("vectordrawable_modules");
-        }
-        return Collections.emptyList();
+    public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
+        List<ViewManager> viewManagers = new ArrayList<>();
+        viewManagers.add(new VectorDrawableManager(reactContext));
+        return viewManagers;
     }
 
     @Override
-    @NonNull
-    public List<ViewManager> createViewManagers(@NonNull final ReactApplicationContext reactContext) {
-        return Collections.singletonList(new VectorDrawableManager(reactContext));
+    public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
+        return Collections.emptyList();
     }
+
 }
